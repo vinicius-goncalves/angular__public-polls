c2166dad77eb54abb076bb0a3b8a5944b5b3968a
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-09-09 20:09:46 -0300

    added helpers and styles

--- src/app/shared/components/poll/poll.component.ts
+++ src/app/shared/components/poll/poll.component.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { PollsService } from '../../../core/services/polls.service';
 import Poll from '../../../types/poll/PollData.interface';
 import { CustomDatePipe } from '../../pipes/custom-date.pipe';
+import { HelpersService } from '../../services/helpers.service';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
@@ -17,6 +18,7 @@ import { ButtonComponent } from '../button/button.component';
         RouterLinkActive,
     ],
     templateUrl: './poll.component.html',
+    styleUrl: './poll.component.css',
 })
 export class PollComponent implements OnChanges {
     @Input({ required: false }) pollData: Partial<Poll> = {
@@ -28,7 +30,10 @@ export class PollComponent implements OnChanges {
         totalVotes: 0,
     };
 
-    constructor(private pollsService: PollsService) {}
+    constructor(
+        private pollsService: PollsService,
+        private helpersService: HelpersService,
+    ) {}
 
     ngOnChanges(changes: SimpleChanges): void {
         this.pollsService
@@ -36,5 +41,15 @@ export class PollComponent implements OnChanges {
             .subscribe((totalVotes) => {
                 this.pollData.totalVotes = totalVotes;
             });
+
+        this.pollData.description = this.helpersService.short(
+            this.pollData.description!,
+            64,
+        );
+
+        this.pollData.title = this.helpersService.short(
+            this.pollData.title!,
+            64,
+        );
     }
 }
