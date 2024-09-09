5da75254efa2640736f335b04ce26f0e4339da1c
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-09-09 19:39:12 -0300

    interface with poll props

--- src/app/types/poll/PollData.interface.ts
+++ src/app/types/poll/PollData.interface.ts
@@ -1,8 +1,15 @@
-export default interface PollData {
+export default interface Poll {
     id: ReturnType<typeof crypto.randomUUID> | string;
     title: string;
     description: string;
     totalVotes: number;
-    expiresAt: Date;
-    createdAt: Date;
+    questions: Question[];
+    createdAt: Date | string;
+    expiresAt: Date | string;
+}
+
+export interface Question {
+    id: ReturnType<typeof crypto.randomUUID> | string;
+    question: string;
+    votes: number;
 }
